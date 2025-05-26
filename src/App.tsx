6f6a7bec5951780b5063a58c4f6d39cc96fc6cec
felixcao <felixcao37@gmail.com>
2025-05-26 21:54:08 +1000

    draft lr movement completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { KeyboardControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience'
+import { Leva } from 'leva'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <KeyboardControls
+				map={ [
+						{ name: 'forward', keys: [ 'ArrowUp', 'KeyW' ] },
+						{ name: 'backward', keys: [ 'ArrowDown', 'KeyS' ] },
+						{ name: 'leftward', keys: [ 'ArrowLeft', 'KeyA' ] },
+						{ name: 'rightward', keys: [ 'ArrowRight', 'KeyD' ] },
+				] }
+			>
+				<Leva />
+				<Canvas
+					shadows
+					camera={ {
+							fov: 45,
+							near: 0.1,
+							far: 200,
+							position: [ -5, 5, 9 ],
+							rotation: [ -.2, -.3, 0],
+					} }
+				>
+					<Experience />
+				</Canvas>
+			</KeyboardControls>
     </>
   )
 }
---
+++ src/Experience.tsx
@@ -0,0 +1,37 @@
+import { OrbitControls } from "@react-three/drei";
+import { University } from "./models";
+import { useControls } from "leva";
+import { useFrame } from "@react-three/fiber";
+import { Vector3 } from "three";
+
+export default function Experience()
+{
+	const { 
+		cameraPosition, 
+		cameraRotation
+	} = useControls({ cameraPosition: {
+		value: {"x":-5,"y":4,"z":9},
+		step: 0.01
+	}, cameraRotation: {
+		value: { x: -.2, y: -.3, z: 0 },
+		step: 0.01
+	}})
+
+	useFrame((state) => {
+		// state.camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z)
+		// state.camera.rotation.set(cameraRotation.x, cameraRotation.y, cameraRotation.z)
+		// state.camera.updateProjectionMatrix();
+	})
+
+	return (
+		<>
+			<OrbitControls />
+			<University />
+			{/* <mesh position={[0,0,0]}>
+				<meshBasicMaterial color={0x00ff00} />
+				<boxGeometry args={[1,1,1]} />
+			</mesh> */}
+			<ambientLight intensity={1}/>
+		</>
+	)
+}
